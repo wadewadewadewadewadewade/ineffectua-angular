c13c85ff6df160b54acb775934f0163e7f207d27
wadewadewadewadewadewade <wade.wade.wade.wade.wade.wade@gmail.com>
2020-04-03 22:12:57 -0700

    Drag-position of the painlog mark is now possible! next is adding, eleeting, editing, and detail view of mark.

--- src/app/tab2/painlog.page.ts
+++ src/app/tab2/painlog.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostListener, Directive } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, HostListener } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { map } from 'rxjs/operators';
@@ -25,6 +25,8 @@ export class PainLogPage implements OnInit {
 
   public locations = new Observable<Location[]>();
 
+  @ViewChild("body") body: ElementRef;
+
   constructor(
     private db: AngularFireDatabase,
     public alertController: AlertController,
@@ -48,12 +50,14 @@ export class PainLogPage implements OnInit {
     });
   }
 
-  mouseup($event: MouseEvent, loc: Location) {
-    const temp = this.getCordinates($event, true);
-    loc.x = temp.x;
-    loc.y = temp.y;
-    console.log(loc, $event);
-    // this.addLocation(loc);
+  stoppedDragging(mark: HTMLElement, location: Location) {
+    const match = mark.style.transform.match(/([0-9\.-]+)px,\s*([0-9\.-]+)px/);
+    if (match && match.length > 1) {
+      location.x = this.numberPairToPercent((((parseFloat(location.x) / 100) * (this.body.nativeElement as HTMLElement).offsetWidth) + parseFloat(match[1])), (this.body.nativeElement as HTMLElement).offsetWidth) + '%';
+      location.y = this.numberPairToPercent((((parseFloat(location.y) / 100) * (this.body.nativeElement as HTMLElement).offsetHeight) + parseFloat(match[2])), (this.body.nativeElement as HTMLElement).offsetHeight) + '%';
+      mark.style.transform = '';
+      this.addLocation(location);
+    }
   }
 
   getNowDateIsoString() {
@@ -87,15 +91,38 @@ export class PainLogPage implements OnInit {
     };
   }
 
-  getCordinates($event: MouseEvent, isMark: boolean): Location {
-    const body = $event.target as HTMLElement,
-      container = isMark ? body.parentElement.parentElement.parentElement : body.parentElement.parentElement,
-      containerWidth = container.offsetWidth,
-      containerHeight = container.offsetHeight,
-      xAsPercent = Math.round((($event.clientX / containerWidth)  + Number.EPSILON) * 10000) / 100,
-      yAsPercent = Math.round((($event.clientY / containerHeight)  + Number.EPSILON) * 10000) / 100,
+  numberPairToPercent(px: number, dimension: number) {
+    return Math.round(((px / dimension)  + Number.EPSILON) * 10000) / 100;
+  }
+
+  getCordinatesMouse($event: MouseEvent, markMatch: Array<any>): Location {
+    const target = $event.target as HTMLElement,
+      isMark = markMatch && markMatch.length > 1,
+      xAsPx = isMark ? $event.clientX - markMatch[1] : $event.clientX,
+      yAsPx = isMark ? $event.clientY - markMatch[2] : $event.clientY,
+      xAsPercent = this.numberPairToPercent(xAsPx, (this.body.nativeElement as HTMLElement).offsetWidth),
+      yAsPercent = this.numberPairToPercent(yAsPx, (this.body.nativeElement as HTMLElement).offsetHeight),
       loc = this.getLocation(xAsPercent + '%', yAsPercent + '%');
     return loc;
+  }
+
+  getCordinatesTouch($event: TouchEvent, markMatch: Array<any>): Location {
+    const target = $event.target as HTMLElement,
+      isMark = markMatch && markMatch.length > 1,
+      xAsPx = isMark ? $event.targetTouches[0].clientX - markMatch[1] : $event.targetTouches[0].clientX,
+      yAsPx = isMark ? $event.targetTouches[0].clientY - markMatch[2] : $event.targetTouches[0].clientY,
+      xAsPercent = Math.round(((xAsPx / (this.body.nativeElement as HTMLElement).offsetWidth)  + Number.EPSILON) * 10000) / 100,
+      yAsPercent = Math.round(((yAsPx / (this.body.nativeElement as HTMLElement).offsetHeight)  + Number.EPSILON) * 10000) / 100,
+      loc = this.getLocation(xAsPercent + '%', yAsPercent + '%');
+    return loc;
+  }
+
+  addLocationMouse($event: MouseEvent) {
+    this.addLocation(this.getCordinatesMouse($event, null));
+  }
+
+  addLocationTouch($event: TouchEvent) {
+    this.addLocation(this.getCordinatesTouch($event, null));
   }
 
   addLocation(loc: Location) {
