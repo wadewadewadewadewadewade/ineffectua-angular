be923566f8545f305e9e1effa96e420f05c23ff3
Wade McDaniel <wade@airmac.local>
2020-04-30 06:19:33 -0700

    Subed out the AngularFire modules, but still no joy

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,3 +1,4 @@
+import { FirebaseDataService } from './services/firebasedata.service';
 import { environment } from 'src/environments/environment';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
@@ -5,9 +6,13 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { Platform, ModalController } from '@ionic/angular';
+import { BrowserModule, TransferState, BrowserTransferStateModule } from '@angular/platform-browser';
+import { BehaviorSubject } from 'rxjs';
+import { Credentials } from './services/firebasedata.service';
 import { AngularFireModule } from '@angular/fire';
-import { AngularFireDatabase, AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireDatabase, AngularFireDatabaseModule, QueryFn } from '@angular/fire/database';
 import { AngularFireAuth, AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireAnalytics, AngularFireAnalyticsModule, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
@@ -16,7 +21,32 @@ describe('AppComponent', () => {
   const platformSpy = { ready: (): Promise<string> => new Promise<string>((resolve, reject) => resolve('ready')) },
     modalControllerSpy = {dismiss: () => { }},
     statusBarSpy = {styleDefault: (): void => undefined},
-    splashScreenSpy = {hide: (): void => undefined};
+    splashScreenSpy = {hide: (): void => undefined},
+    firebaseDataserviceStub = {
+      isLoggedIn: () => new Promise<firebase.User>((resolve, _reject) => resolve()),
+      saveUserAccountInformation: (res: firebase.User) => { return },
+      registerUser: (value: Credentials) =>  new Promise<any>((resolve, reject) => resolve()),
+      loginUser: (value: Credentials) =>  new Promise<any>((resolve, reject) => resolve()),
+      logoutUser: () =>  new Promise<any>((resolve, reject) => resolve()),
+      get: (collection: string, orderby?: QueryFn) => new BehaviorSubject<[]>([]),
+      getItem: (collection: string, uid: string) => new Promise<any>((resolve, reject) => resolve()),
+      put: (collection: string, val: any, orderby?: QueryFn) => new Promise<void>((resolve, reject) => resolve()),
+      remove: (collection: string, val: any) => new Promise<void>((resolve, reject) => resolve()),
+      observe: (success: any) => { return }
+    }, authStub = {
+      onAuthStateChanged: (user: firebase.User) => { return },
+      signOut: () => new Promise<any>((resolve, reject) => resolve()),
+      signInWithEmailAndPassword: (email: string, password: string) => new Promise<any>((resolve, reject) => resolve()),
+      createUserWithEmailAndPassword: (email: string, password: string) => new Promise<any>((resolve, reject) => resolve()),
+    }, databaseStub = {
+      object: (path: string) => { return },
+      list: (collection: string, orderby?: QueryFn) => ({
+        snapshotChanges: () => new BehaviorSubject<[]>([]),
+        remove: (val: string) => { return },
+      }),
+    }, analyticsStub = {
+
+    };
 
   beforeEach(async(() => {
     platformReadySpy = Promise.resolve();
@@ -29,12 +59,19 @@ describe('AppComponent', () => {
         { provide: StatusBar, useValue: statusBarSpy },
         { provide: SplashScreen, useValue: splashScreenSpy },
         { provide: ModalController, useValue: modalControllerSpy },
+        { provide: FirebaseDataService, useValue: firebaseDataserviceStub },
+        { provide: AngularFireAuthModule, useValue: authStub },
+        { provide: AngularFireDatabaseModule, useValue: databaseStub },
+        { provide: AngularFireAnalyticsModule, useValue: analyticsStub },
+        TransferState,
+        BrowserTransferStateModule,
+        ScreenTrackingService,
+        UserTrackingService,
       ],
       imports: [
         RouterTestingModule,
         AngularFireModule.initializeApp(environment.firebase),
-        AngularFireAuthModule,
-        AngularFireDatabaseModule,
+        BrowserModule.withServerTransition({ appId: 'serverApp' }),
       ]
     }).compileComponents();
   }));
--- src/app/services/firebasedata.service.spec.ts
+++ src/app/services/firebasedata.service.spec.ts
@@ -5,7 +5,6 @@ import { BrowserTransferStateModule } from '@angular/platform-browser';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('FirebaseDataService', () => {
-  const rando = () => (Math.random() + 1).toString(36).substring(7);
 
   beforeEach(() => TestBed.configureTestingModule({
     schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
